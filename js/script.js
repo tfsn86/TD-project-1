a758ed2d80670c5bc7791ffb0043583fd2a58fa4
Torben Schytt-Nielsen <tfsn86@gmail.com>
2018-12-10 19:16:46 +0100

    Fixed an issue in the printQuote function
    The issue was that the source, citation etc. was not inline. Now it is fixed by moving the closing </p>

--- js/script.js
+++ js/script.js
@@ -19,32 +19,39 @@ var quotes = [
   {
     quote: 'Success consists of going from failure to failure without loss of enthusiasm.',
     source: 'Winston Churchill',
+    genre: 'Political quote'
   },
   {
     quote: 'Pleasure in the job puts perfection in the work.',
-    source: 'Aristotle'
+    source: 'Aristotle',
+    genre: 'Philosophical quote'
   },
   {
     quote: 'Wise men speak because they have something to say; fools because they have to say something.',
-    source: 'Plato'
+    source: 'Plato',
+    genre: 'Philosophical quote'
   },
   {
     quote: 'I not only use all the brains that I have, but all I can borrow.',
-    source: 'Woodrow Wilson'
+    source: 'Woodrow Wilson',
+    genre: 'Political quote'
   },
   {
     quote: "Patience is a virtue, and I'm learning patience. It's a tough lesson.",
-    source: 'Elon Musk'
+    source: 'Elon Musk',
+    genre: 'Technology quote'
   },
   {
     quote: 'Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.',
     source: 'Patrick McKenzie',
+    genre: 'Technology quote',
     citation: 'Twitter',
-    year: '2016'
+    year: '2016',
   },
   {
     quote: "I don't think that you can score as spectacular a goal as those of Zlatan in a video game - even though these games are very realistic these days.",
-    source: 'Zlatan Ibrahimovic'
+    source: 'Zlatan Ibrahimovic',
+    genre: 'Sports quote'
   }
 ]
 
@@ -89,13 +96,15 @@ function printQuote() {
   var randomQuote = getRandomQuote(quotes);
   HTML = '';
   HTML += '<p class="quote">' + randomQuote.quote + '</p>';
-  HTML += '<p class="source">' + randomQuote.source + '</p>';
+  HTML += '<p class="source">' + randomQuote.source;
   if (randomQuote.citation !== undefined) {
     HTML += '<span class="citation">' + randomQuote.citation + '</span>';    
   }
   if (randomQuote.year !== undefined) {
     HTML += '<span class="year">' + randomQuote.year + '</span>';
   }
+  HTML += '<span class="genre">' + randomQuote.genre + '</span>';
+  HTML += '</p>';
   document.getElementById("quote-box").innerHTML = HTML;
 }
 
